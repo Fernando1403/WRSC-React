df64ec063fbbc12a532dce79d9e5ecfd340f2c7f
Fernando <84139372+Fernando1403@users.noreply.github.com>
2024-10-11 16:58:39 -0300

    Add files via upload

--- src/Components/FormContato.jsx
+++ src/Components/FormContato.jsx
@@ -1,4 +1,48 @@
+import { useState } from "react";
+import emailjs from '@emailjs/browser'
+
 function FormContato() {
+
+    const [pessoa, SetPessoa] = useState('')
+    const [empresa, SetEmpresa] = useState('')
+    const [email, SetEmail] = useState('')
+    const [telefone, SetTelefone] = useState('')
+    const [assunto, SetAssunto] = useState('')
+    const [cargo, SetCargo] = useState('')
+    const [estado, SetEstado] = useState('')
+    const [mensagem, SetMensagem] = useState('')
+
+    function sendEmail(e){
+        e.preventDefault();
+
+        const templateParams = {
+            from_name: pessoa,
+            pessoa: pessoa,
+            empresa: empresa,
+            email: email,
+            telefone: telefone,
+            assunto: assunto,
+            cargo: cargo,
+            estado: estado,
+            mensagem: mensagem
+        }
+
+        emailjs.send("service_srbkuso","template_6vesvh9", templateParams, "r5haNZZbpwNSoMdz1")
+        .then((resposta) => {
+            alert("Email enviado com sucesso!")
+            SetPessoa('')
+            SetEmpresa('')
+            SetEmail('')
+            SetTelefone('')
+            SetAssunto('')
+            SetCargo('')
+            SetEstado('')
+            SetMensagem('')
+        }, (err) => {
+            alert("Erro ao enviar", err)
+        })
+    }
+
     return ( 
         <>
             <div class="container-contato">
@@ -9,23 +53,23 @@ function FormContato() {
                             <p>Solicite um orçamento pelo formulário ao lado ou entre em contato conosco pelo numero (11) 98801-3288 ou pelo email contato@wrsc.com.br</p>
                         </div>
                         <div class="direita-contato">
-                            <form action="" class="form-contato">
-                                <input type="text" placeholder="Pessoa"/>
-                                <input type="text" placeholder="Empresa"/>
-                                <input type="email" name="" id="" placeholder="Email"/>
-                                <input type="text" name="" id="" placeholder="Telefone"/>
-                                <select name="Assunto" id="">
+                            <form action="" class="form-contato" onSubmit={sendEmail}>
+                                <input type="text" placeholder="Pessoa" onChange={(e) => SetPessoa(e.target.value)} value={pessoa} required/>
+                                <input type="text" placeholder="Empresa" onChange={(e) => SetEmpresa(e.target.value)} value={empresa} required/>
+                                <input type="email" name="" id="" placeholder="Email" onChange={(e) => SetEmail(e.target.value)} value={email} required/>
+                                <input type="text" name="" id="" placeholder="Telefone" onChange={(e) => SetTelefone(e.target.value)} value={telefone}/>
+                                <select name="Assunto" id="" onChange={(e) => SetAssunto(e.target.value)} value={assunto} required>
                                     <option value="" disabled selected>Assunto</option>
-                                    <option value="">Gostaria de Conhecer Mais</option>
-                                    <option value="">Proposta Comercial -  Setor Privado</option>
-                                    <option value="">Proposta Comercial -   Setor Publico</option>
-                                    <option value="">Tenho uma demanda Urgente de Dynamics 365</option>
-                                    <option value="">Tenho uma demanda Urgente de Ferramentas Microsoft</option>
-                                    <option value="">Vídeo Conferência - Sistemas e Empresa</option>
-                                    <option value="">Outros Assuntos</option>
+                                    <option value="Gostaria de Conhecer Mais">Gostaria de Conhecer Mais</option>
+                                    <option value="Proposta Comercial -  Setor Privado">Proposta Comercial -  Setor Privado</option>
+                                    <option value="Proposta Comercial -   Setor Publico">Proposta Comercial -   Setor Publico</option>
+                                    <option value="Tenho uma demanda Urgente de Dynamics 365">Tenho uma demanda Urgente de Dynamics 365</option>
+                                    <option value="Tenho uma demanda Urgente de Ferramentas Microsoft">Tenho uma demanda Urgente de Ferramentas Microsoft</option>
+                                    <option value="Vídeo Conferência - Sistemas e Empresa">Vídeo Conferência - Sistemas e Empresa</option>
+                                    <option value="Outros Assuntos">Outros Assuntos</option>
                                 </select>
-                                <input type="text" placeholder="Cargo"/>
-                                <select name="Estado" id="">
+                                <input type="text" placeholder="Cargo" onChange={(e) => SetCargo(e.target.value)} value={cargo}/>
+                                <select name="Estado" id="" onChange={(e) => SetEstado(e.target.value)} value={estado}>
                                     <option value="" disabled selected>Selecione um estado</option>
                                     <option value="AC">Acre (AC)</option>
                                     <option value="AL">Alagoas (AL)</option>
@@ -55,8 +99,8 @@ function FormContato() {
                                     <option value="SE">Sergipe (SE)</option>
                                     <option value="TO">Tocantins (TO)</option>
                                 </select>
-                                <textarea name="" id="" placeholder="Mensagem Livre"></textarea>
-                                <input type="button" value="Enviar"/>
+                                <textarea name="" id="" placeholder="Mensagem Livre" onChange={(e) => SetMensagem(e.target.value)} value={mensagem}></textarea>
+                                <input type="submit" value="Enviar"/>
                             </form>
                         </div>
                     </div>
